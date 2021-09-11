b84acd0e90a3b3608f3940ac545d5a9353191d4e
RohanKotagiri <rohankotagiri10@gmail.com>
2021-09-11 19:02:06 +0530

    final commit

---
+++ video_streamlit.py
@@ -0,0 +1,16 @@
+import streamlit as st
+import numpy as np
+import cv2
+
+st.write("""
+# Video Display 
+""")
+
+switch = st.select_slider("Video",options=["OFF","ON"],value="OFF")
+position = st.empty()
+if switch=="ON":
+    vid = cv2.VideoCapture(0)
+    while(True):
+        _, frame = vid.read()
+        frame = cv2.cvtColor(frame,cv2.COLOR_BGR2RGB)
+        position.image(frame)
